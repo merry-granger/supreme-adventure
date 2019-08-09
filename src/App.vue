ac2d5cd581d36ace885a294cb5b3f2e2648ec301
Mariia <monny.grainger@gmail.com>
2019-08-09 16:36:42 +0300

    app

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,77 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to World of Hot-Dogs"/>
+    Hot Dogs
+    <div><button onclick="add">Add new hot dog</button>
+    </div>
+    <HotDogsList :hotDogs="hotDogs"/>
+    <HotDogForm :hotDog="hotDog" :formMode="formMode" :formDialog="formDialog" @unshift="unshiftToArray" @edit="editInArr"/>
+
+            
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import hotDogForms from './components/hotDogForms'
+import hotDogItem from './components/hotDogItem'
+import hotDogList from './components/hotDogList'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
-  }
+    hotDogForms,
+    hotDogList
+  },
+  provide() {
+           return {
+              update: () => this.getHotDogs(),
+                hotDogAction: (hotDog, formMode, formDialog) => {
+                    this.hotDog = {...hotDog};
+                    this.formMode = formMode;
+                    this.formDialog = formDialog;
+                },
+                deleteFromArr: (hotDog) => {
+                    this.hotDogs = this.hotDogs.filter(item => item._id !== hotDog._id);
+                }
+            }
+        },
+    data: () => ({
+            hotDogs: [],
+            index: 0,
+            hotDog: {
+                title: ""
+            },
+            formMode: "",
+            formDialog: false,
+        }),
+    methods: {
+            async add() {
+                this.formDialog = true;
+                this.formMode = 'adding';
+            },
+            async getHotDogs() {
+                let url = 'bibka';
+                try {
+                    let response = await axios.get(url);
+                    this.hotDogs = response.data.reverse();
+                } catch (e) {
+                    alert(e.message);
+                }
+            },
+            unshiftArray(hotDog) {
+                this.hotDogs.unshift(hotDog);
+            },
+            editInArr(hotDog) {
+                this.hotDogs.find(item => {
+                    if (item._id === hotDog._id)
+                        item.title = hotDog.title;
+                });
+            },
+        },
+      created() {
+            this.getHotDogs();
+        },
 }
+
 </script>
 
 <style>
@@ -22,7 +80,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #1b9954;
   margin-top: 60px;
 }
 </style>
